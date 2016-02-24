ea047d557818e5b7d4175c759b15bcd591617056
yumeijin <yuj019>
2016-02-23 23:24:05 -0800

    Initialize the tower; add start, quit, pause menu

---
+++ Assets/block.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class block : MonoBehaviour {
+
+	public Transform singleBlock;
+
+	void Start() {
+		for (int y = 0; y < 50; y++) {
+			for (int x = 0; x < 3; x++) {
+				for (int z = 0; z < 3; z++) {
+					Instantiate (singleBlock, new Vector3 (x, y, z), Quaternion.identity);
+				}
+			}
+		}
+	}
+}
