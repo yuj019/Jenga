a08225f6edd8dd9f46bfc0d751af2ade9751719e
S1Whang <s1whang@ucsd.edu>
2016-02-25 18:21:15 -0800

    Changed Block.CS

--- Assets/block.cs
+++ Assets/block.cs
@@ -4,13 +4,19 @@ using System.Collections;
 public class block : MonoBehaviour {
 
 	public Transform singleBlock;
-
 	void Start() {
 		for (int y = 0; y < 50; y++) {
+			float pos = -.25f;
+			Quaternion rot = Quaternion.identity;
 			for (int x = 0; x < 3; x++) {
-				for (int z = 0; z < 3; z++) {
-					Instantiate (singleBlock, new Vector3 (x, y, z), Quaternion.identity);
+				if (y % 2 == 0) {
+					rot = Quaternion.identity;
+					Instantiate (singleBlock, new Vector3 (0,(.18f*y),pos), rot);
+				} else {
+					rot.eulerAngles = new Vector3 (0, 90, 0);
+					Instantiate (singleBlock, new Vector3 (pos,(.18f*y),0), rot);
 				}
+				pos += .25f;
 			}
 		}
 	}
