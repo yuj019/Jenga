bb975ccee9751d699a4237d90171c5aada4f8b92
yumeijin <yuj019>
2016-02-26 14:21:40 -0800

    Updated menu

--- Assets/block.cs
+++ Assets/block.cs
@@ -8,22 +8,54 @@ public class block : MonoBehaviour {
 	public Transform singleBlock;
 	public Vector3 torque;
 	public Rigidbody rb;
+	public InputField input;
+	string txt;
+	int height = 18;
 
+	//press "esc" to show the pulse panel
 	void Start() {
+		
 		rb = singleBlock.GetComponent<Rigidbody>();
-		for (int y = 0; y < 50; y++) {
-			float pos = .25f;
-			Quaternion rot = Quaternion.identity;
-			for (int x = 0; x < 3; x++) {
-				if (y % 2 == 0) {
-					rot = Quaternion.identity;
-					Instantiate (singleBlock, new Vector3 (0,(.18f*y),pos), rot);
-					transform.localScale = Vector3.one * Random.Range(1, 5);
-				} else {
-					rot.eulerAngles = new Vector3 (0, 90, 0);
-					Instantiate (singleBlock, new Vector3 (pos,(.18f*y),0), rot);
-					transform.localScale = Vector3.one * Random.Range(1, 5);
+		//set default height to 18
+			for (int y = 0; y < height; y++) {
+				float pos = .25f;
+				Quaternion rot = Quaternion.identity;
+				for (int x = 0; x < 3; x++) {
+					if (y % 2 == 0) {
+						rot = Quaternion.identity;
+						Instantiate (singleBlock, new Vector3 (0, (.18f * y), pos), rot);
+						transform.localScale = Vector3.one * Random.Range (1, 5);
+					} else {
+						rot.eulerAngles = new Vector3 (0, 90, 0);
+						Instantiate (singleBlock, new Vector3 (pos, (.18f * y), 0), rot);
+						transform.localScale = Vector3.one * Random.Range (1, 5);
 					}
+				}
+			}
+		if (transform.hasChanged) {
+			//let user select height in between 2 and 50
+			input = singleBlock.GetComponent<InputField>();
+			txt = input.text;
+			height = int.Parse (txt);
+
+			if (height > 1 && height < 51) {
+				for (int y = 0; y < height; y++) {
+					float pos = .25f;
+					Quaternion rot = Quaternion.identity;
+					for (int x = 0; x < 3; x++) {
+						if (y % 2 == 0) {
+							rot = Quaternion.identity;
+							Instantiate (singleBlock, new Vector3 (0, (.18f * y), pos), rot);
+							transform.localScale = Vector3.one * Random.Range (1, 5);
+						} else {
+							rot.eulerAngles = new Vector3 (0, 90, 0);
+							Instantiate (singleBlock, new Vector3 (pos, (.18f * y), 0), rot);
+							transform.localScale = Vector3.one * Random.Range (1, 5);
+						}
+					}
+				}
+			} else {
+				Debug.Log ("Please select a height in between 2 and 50");
 			}
 		}
 	}
@@ -34,7 +66,8 @@ public class block : MonoBehaviour {
 			GameObject clone = InstantiateRandomScale(prefab, 1, 5);
 		}
 	}*/
-	void update() {
+	/*void update() {
+
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			SceneManager.LoadScene ("Jenga");
 		}
--- Assets/restart.cs
+++ Assets/restart.cs
@@ -3,7 +3,8 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class restart : MonoBehaviour {
-	
+
+	//press "esc" to show the pulse panel
 	// Update is called once per frame
 	void update() {
 		if (Input.GetKeyDown (KeyCode.Space)) {
